0f69a0b30a81bd138962f93d749e2cdb02d70d45
Cristina Mateo <cris.mateo.99@gmail.com>
2023-10-03 12:48:06 +0200

    más validacion de formulario con alerta de error

--- demo_web/script.js
+++ demo_web/script.js
@@ -23,35 +23,60 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     //lo importante de esto es cómo controlar el envio del formualrio. Cómo parar el formulario para hacer el chequeo previo, etc.
 
 
-    const fname = event.target.fname.value //3<fname<30 required
-    const lname = event.target.lname.value //3<lname<30 required
-    const email = event.target.email.value //.com .es @ required
-    const tlf= event.target.tlf.value // number  y 9 valores required
+    const fname = event.target.fname.value //3<fname<30 
+    const lname = event.target.lname.value //3<lname<30 
+    const email = event.target.email.value //.com .es @ 
+    const tlf= event.target.tlf.value // number  y 9 valores 
     const favorite = event.target.favorite.value //required?
     const season = event.target.season.value //required
     const accept =event.target.accept.checked //required
     const spam =event.target.spam.checked //required
     const comments =event.target.comments.value
 
+
+let msj ="";
+
+
+
     if(fname.length <3|| fname.length > 30){
-        console.log("Nombre fuera del tamaño: 3-30") //también puede ser una alerta, pero no abusar de las alertas!
+        console.log("Nombre fuera del tamaño: 3-30"); //también puede ser una alerta, pero no abusar de las alertas!
+        msj+="Nombre fuera del tamaño: 3-30\n";
     }
     if(lname.length <3|| lname.length > 30){
-        console.log("Apellido fuera del tamaño: 3-30")
+        console.log("Apellido fuera del tamaño: 3-30");
+        msj+"Apellido fuera del tamaño: 3-30\n";
     }
 
     if((!email.endsWith(".com") && !email.endsWith(".es")) || !email.includes("@")){
         console.log("Error validación" + email);
+        msj+= "Error validación" + email +"\n";
     }
 
-    if(tlf.length != 11 && typeof tlf != Number){
+    if(tlf.length != 9 && typeof tlf != Number){
         console.log("Error validación" + tlf);
+        msj+= "Error validación" + tlf +"\n";
+    }
+
+    if(season ==""){
+        console.log("Selecciona alguna estación del año");
+        msj+="Selecciona alguna estación del año \n";
+    }
+
+    if(!spam || !accept){
+        console.log("Acepta condiciones o envío de publicidad");
+        msj+="Acepta condiciones o envío de publicidad\n";
     }
 
 
+    //Comprobar si hay errores
+    if(msj.length != 0){
+        alert(msj); //imprime mensaje final de error
 
+        let parrafo = document.createElement("p");
+        let mensaje= document.createTextNode(msj);
+        parrafo.appendChild(mensaje);
 
-
- 
+        document.getElementById("contact").appendChild(parrafo);//dibuja resultado
+    }
 
 })
